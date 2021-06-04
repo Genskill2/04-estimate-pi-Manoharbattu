18bc462fc00f2732b622ecdf61fa95762370e9e7
Manoharbattu <85050524+Manoharbattu@users.noreply.github.com>
2021-06-04 22:21:11 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -1,4 +1,4 @@
-#include <assert.h>
+include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -39,5 +39,38 @@ int main(void) {
   }
 }
 
-
-
+float mc_pi(int n){
+  float x,y;
+  int i=0,circle=0;
+  float z;
+  int square=0;
+  
+  float pi;
+  
+  while (i<n-1){
+   
+   x=frandom();
+   y=frandom();
+   z=x*x+y*y;
+  
+   
+   if(z<=1){
+     circle+=1;
+     square+=1;
+     }
+   else{
+     square+=1;
+   
+   i++;
+     
+   }
+   
+   }
+ 
+ pi=(float)4*circle/square;
+ return pi;
+   
+    
+  
+  
+}
